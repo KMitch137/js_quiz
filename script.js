e0b56b9ccc9b0909eff811e130c2a1f2f7b0276d
kevin mitchell <kmitch137@gmail.com>
2023-05-04 21:14:24 -0700

    added a timer to js. Not yet styled properly

---
+++ script.js
@@ -0,0 +1,47 @@
+var startButton = document.getElementById("start-button")
+var startContainer = document.querySelector(".start-container")
+var questionContainer = document.querySelector(".question-container")
+var timerEl = document.getElementById('countdown');
+var mainEl = document.getElementById('timer')
+
+var question1 = {
+    title: "Are semicolons bad?",
+    choices: ["Yes", "No", "Sometimes"],
+    answer: "No",
+}
+
+var question2 = {
+    title: "Are question marks bad?",
+    choices: ["Yes", "No", "Sometimes"],
+    answer: "No",
+}
+
+var question3 = {
+    title: "Are hashtags bad?",
+    choices: ["Yes", "No", "Sometimes"],
+    answer: "No",
+}
+
+var questions = [question1, question2, question3]
+
+function startQuiz() {
+    startContainer.style.display = "none"
+    questionContainer.style.display = "block"
+}
+
+startButton.addEventListener("click", startQuiz, countdown)
+
+function countdown() {
+    var timeLeft = 15;
+    var timeInterval = setInterval(function () {
+      if (timeLeft > 1) {
+        timerEl.textContent = timeLeft;
+        timeLeft--;
+      } else {
+        timerEl.textContent = '';
+        clearInterval(timeInterval);
+      }
+    }, 1000);
+  }
+  startButton.addEventListener("click", countdown)
+
