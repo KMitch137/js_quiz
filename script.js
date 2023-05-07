85599a6f9118b5ca6d7ea0a9660a197edf9eeb52
kevin mitchell <kmitch137@gmail.com>
2023-05-06 21:43:18 -0700

    added secondary html and css to test a new approach

--- script.js
+++ script.js
@@ -5,43 +5,42 @@ var timerEl = document.getElementById('countdown');
 var mainEl = document.getElementById('timer')
 
 var question1 = {
-    title: "Are semicolons bad?",
-    choices: ["Yes", "No", "Sometimes"],
-    answer: "No",
+  title: "Are semicolons bad?",
+  choices: ["Yes", "No", "Sometimes"],
+  answer: "No",
 }
 
 var question2 = {
-    title: "Are question marks bad?",
-    choices: ["Yes", "No", "Sometimes"],
-    answer: "No",
+  title: "Are question marks bad?",
+  choices: ["Yes", "No", "Sometimes"],
+  answer: "No",
 }
 
 var question3 = {
-    title: "Are hashtags bad?",
-    choices: ["Yes", "No", "Sometimes"],
-    answer: "No",
+  title: "Are hashtags bad?",
+  choices: ["Yes", "No", "Sometimes"],
+  answer: "No",
 }
 
 var questions = [question1, question2, question3]
 
 function startQuiz() {
-    startContainer.style.display = "none"
-    questionContainer.style.display = "block"
+  startContainer.style.display = "none"
+  questionContainer.style.display = "block"
 }
 
 startButton.addEventListener("click", startQuiz, countdown)
 
 function countdown() {
-    var timeLeft = 15;
-    var timeInterval = setInterval(function () {
-      if (timeLeft > 1) {
-        timerEl.textContent = timeLeft;
-        timeLeft--;
-      } else {
-        timerEl.textContent = '';
-        clearInterval(timeInterval);
-      }
-    }, 1000);
-  }
-  startButton.addEventListener("click", countdown)
-
+  var timeLeft = 120;
+  var timeInterval = setInterval(function () {
+    if (timeLeft > 1) {
+      timerEl.textContent = "Seconds remaining: " + timeLeft;
+      timeLeft--;
+    } else {
+      timerEl.textContent = 'Game Over';
+      clearInterval(timeInterval);
+    }
+  }, 1000);
+}
+startButton.addEventListener("click", countdown)
